93ec45b17a3b7100bf581b1358cd0dffc52cd5a2
Maksim Vasilyev <m.vasilyev@sweedpos.com>
2025-01-24 22:39:41 +0400

    web server

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 // import "." from 'fs:';
 import * as cheerio from 'cheerio';
 
-
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const URL = 'https://cinemastercard.az/';
-  let resp;
-  try {
-    resp = await fetch(URL);
-  } catch (e) {
-    console.log(e);
-  }
-  const html = await resp?.text();
-  const $ = cheerio.load(html);
-  console.log($('footer a').first().text());
+  await Deno.serve({ port: 3000 }, async () => {
+    const URL = 'https://cinemastercard.az/';
+    let resp;
+    try {
+      resp = await fetch(URL);
+    } catch (e) {
+      console.log('error: ', e);
+    }
+    const html = (await resp?.text()) ?? "";
+    const $ = cheerio.load(html);
+    const result = $('footer a').first().text();
+    return new Response(result, {
+      headers: { "Content-Type": "text/plain" },
+    });
+  });
 }
