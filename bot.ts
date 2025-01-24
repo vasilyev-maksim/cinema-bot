a55687a60bd3a5a58eac2f2a24c9af5d62205ba7
Maksim Vasilyev <m.vasilyev@sweedpos.com>
2025-01-24 22:50:05 +0400

    bot.ts

---
+++ bot.ts
@@ -0,0 +1,18 @@
+import { Bot } from "https://deno.land/x/grammy@v1.17.0/mod.ts";
+
+// Ваш токен, полученный от BotFather
+const bot = new Bot("7717489452:AAELJ4zQkAGVA6NTTWVlOUzKaMnDcwb832w");
+
+// Команда /start
+bot.command("start", (ctx) => {
+  ctx.reply("Привет! Я ваш Telegram-бот на Deno!");
+});
+
+// Ответ на любое сообщение
+bot.on("message", (ctx) => {
+  ctx.reply(`Вы написали: ${ctx.message.text}`);
+});
+
+console.log("Бот запущен! Откройте его в Telegram.");
+// Запускаем бота
+bot.start();
