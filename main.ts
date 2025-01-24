e5f0c754e20dd867b90e17232fa95cb8276ac3cf
Maksim Vasilyev <m.vasilyev@sweedpos.com>
2025-01-24 22:01:05 +0400

    try catch added

--- main.ts
+++ main.ts
@@ -5,8 +5,13 @@ import * as cheerio from 'cheerio';
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   const URL = 'https://cinemastercard.az/';
-  const resp = await fetch(URL);
-  const html = await resp.text();
+  let resp;
+  try {
+    resp = await fetch(URL);
+  } catch (e) {
+    console.log(e);
+  }
+  const html = await resp?.text();
   const $ = cheerio.load(html);
   console.log($('footer a').first().text());
 }
